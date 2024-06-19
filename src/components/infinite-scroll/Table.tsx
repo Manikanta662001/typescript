fba61fa729c5e796ac2cda55be4dfd083020b0ff
Vamsi8074 <gundlurimanikanta142@gmail.com>
2024-06-19 20:19:27 +0530

    Added remaining code

--- src/components/infinite-scroll/Table.tsx
+++ src/components/infinite-scroll/Table.tsx
@@ -2,41 +2,46 @@ import React, { useEffect, useRef, useState } from "react";
 import "./Table.css";
 
 interface TableProps {
-  sendingData: string[];
+  initialData: string[];
   loading: boolean;
   loadMore: (pageNum: number) => Promise<string[]>;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 const Table = (props: TableProps) => {
-  const { sendingData, loading, loadMore } = props;
-  const [showingdata, setShowingData] = useState<string[]>(sendingData);
-  const [pageNumber, setPageNumber] = useState<number>(1);
+  const { initialData, loading, loadMore, setLoading } = props;
+  const [showingdata, setShowingData] = useState<string[]>(initialData);
+  const [pageNumber, setPageNumber] = useState<number>(0);
   const scrollRef = useRef<HTMLDivElement>(null);
 
   const handleScroll = async () => {
     const { scrollHeight, scrollTop, clientHeight } =
       scrollRef.current as HTMLDivElement;
-    console.log("SCROLL::::", { scrollHeight, scrollTop, clientHeight });
-    if (scrollTop + clientHeight >= scrollHeight - 3) {
-      setPageNumber(pageNumber + 1);
-      const newData = await loadMore(pageNumber);
-      console.log("DATA:::::::", newData);
-      setShowingData([...showingdata, ...newData]);
+
+    if (scrollTop + clientHeight >= scrollHeight - 3 && !loading) {
+      setPageNumber((prevPageNum) => {
+        const nextPageNum = prevPageNum + 1;
+        const fetchData = async () => {
+          const newData = await loadMore(nextPageNum);
+          if (newData) setShowingData([...showingdata, ...newData]);
+        };
+        fetchData();
+        return nextPageNum;
+      });
     }
   };
-
   useEffect(() => {
     scrollRef.current?.addEventListener("scroll", handleScroll);
     return () => {
       scrollRef.current?.removeEventListener("scroll", handleScroll);
     };
-  }, []);
-
+  }, [loading]);
   return (
     <div ref={scrollRef} className="scroll-list-div">
       <ul>
         {showingdata.map((item, ind) => {
-          return <li>{item}</li>;
+          return <li key={item}>{item}</li>;
         })}
+        {loading && <li>Loading.....</li>}
       </ul>
     </div>
   );
--- src/components/infinite-scroll/index.tsx
+++ src/components/infinite-scroll/index.tsx
@@ -3,24 +3,24 @@ import Table from "./Table";
 
 const totalData = Array.from({ length: 100 }, (_, ind) => `test ${ind + 1}`);
 const InfiniteScroll = () => {
-  const [sendingData, setSendingData] = useState<string[]>(
-    totalData.slice(0, 10)
-  );
+  const initialData = totalData.slice(0, 10);
   const [loading, setLoading] = useState<boolean>(false);
   const loadMore = async (pageNum: number): Promise<string[]> => {
-    console.log("NUM::::", pageNum);
     setLoading(true);
-    await setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-    const x = totalData.slice(pageNum * 10 + 1, pageNum + 1 * 10);
-    console.log(x, "::::::");
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    setLoading(false);
+    const x = totalData.slice(pageNum * 10, (pageNum + 1) * 10);
     return x;
   };
   return (
     <div>
       <h1>InfiniteScroll</h1>
-      <Table sendingData={sendingData} loading={loading} loadMore={loadMore} />
+      <Table
+        initialData={initialData}
+        loading={loading}
+        loadMore={loadMore}
+        setLoading={setLoading}
+      />
     </div>
   );
 };
